0efb1d066a132e1e163b022d43682cd1c25f3fce
Pososhenko <Sergey.Pososhenko@calibresys.com>
2016-03-08 14:28:22 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                Console.WriteLine(string.Format("Hello {0}",args[0]));
+            }
+            else
+            {
+                Console.WriteLine("Hello World");
+            }
+            Console.WriteLine("\nPress any key to exit!");
+            Console.ReadKey();
+        }
+    }
+}
