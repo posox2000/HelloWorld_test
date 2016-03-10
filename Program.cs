3d6e0054244d899d1b5c228932944185278aef45
posox2000 <posox2000@gmail.com>
2016-03-10 12:34:06 -0500

    Some class and UnitTest for it

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HelloWorld_test.Utils;
 
 namespace HelloWorld_test
 {
@@ -10,14 +11,20 @@ namespace HelloWorld_test
     {
         static void Main(string[] args)
         {
+            var myTimeCalculator = new TimeHelper();
+            var days = myTimeCalculator.GetDayOfTheYear(DateTime.Now);
+            
             if (args.Length > 0)
             {
                 Console.WriteLine(string.Format("Hello {0}!",args[0]));
+                
             }
             else
             {
                 Console.WriteLine("Hello World");
             }
+            Console.WriteLine(string.Format("{0} - days after year begins...", days));
+
             Console.WriteLine("\nPress any key to exit!");
             Console.ReadKey();
         }
---
+++ Utils/TimeHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld_test.Utils
+{
+    public class TimeHelper
+    {
+        public double GetDayOfTheYear(System.DateTime date)
+        {
+            var yearOnly = DateTime.ParseExact(date.Year.ToString("0000"),"yyyy", System.Globalization.CultureInfo.InvariantCulture);
+            var result = Math.Floor((date - yearOnly).TotalDays);
+            //result=date.DayOfYear;
+            return result;
+        }
+    }
+}
